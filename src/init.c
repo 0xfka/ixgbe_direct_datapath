39a3cd2e8bd92d15ffdaadf8dd5c953f9d412378
Fatih Karaoğlu <fatih39333@gmail.com>
2026-03-04 22:31:12 +0000

    chore: disable temporarily enabled promiscuous modes & broadcast traffic
    Signed-off-by: Fatih Karaoğlu <fatih39333@gmail.com>

--- src/init.c
+++ src/init.c
@@ -117,10 +117,10 @@ dmaiok:;
   /* Filter Control Registers */
   /* Broadcast was cleared since the ARP will be hardcoded. */
   read_val = ixgbe_read_reg(hw, IXGBE_FCTRL);
-  IXGBE_SET_BITS(read_val, IXGBE_FCTRL_BAM);
+  IXGBE_CLEAR_BITS(read_val, IXGBE_FCTRL_BAM);
   IXGBE_CLEAR_BITS(read_val, IXGBE_FCTRL_MPE);
   IXGBE_CLEAR_BITS(read_val, IXGBE_FCTRL_SBP);
-  IXGBE_SET_BITS(read_val, IXGBE_FCTRL_UPE);
+  IXGBE_CLEAR_BITS(read_val, IXGBE_FCTRL_UPE);
   ixgbe_write_reg(hw,IXGBE_FCTRL,read_val);
   /* Out of scope, disabled. */
   read_val = ixgbe_read_reg(hw, IXGBE_VLNCTRL);
